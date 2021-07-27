2672f799aacf6f8bc9c5351a6c41592fde1dacda
juanmiguel <juanmiguel.lombardisuzzi@gmail.com>
2021-07-27 14:29:18 -0300

    i do not know what i am doing

--- src/vendedores.ts
+++ src/vendedores.ts
@@ -2,6 +2,7 @@ import { any } from "ramda";
 
 abstract class Vendedor {
   abstract puedeTrabajarEn(ciudad: Ciudad): boolean;
+  abstract esInfuyente(): boolean;
 }
 
 export class VendedorFijo extends Vendedor {
@@ -12,6 +13,10 @@ export class VendedorFijo extends Vendedor {
   puedeTrabajarEn(ciudad: Ciudad): boolean {
     return ciudad == this.ciudadOrigen;
   }
+
+  esInfuyente(): boolean {
+    return false;
+  }
 }
 
 export class Viajante extends Vendedor {
@@ -21,6 +26,24 @@ export class Viajante extends Vendedor {
 
   puedeTrabajarEn(ciudad: Ciudad): boolean {
     return any((p) => p == ciudad.provincia, this.provinciasDondeTrabaja);
+  }
+
+  esInfuyente(): boolean {
+    return false;
+  }
+}
+
+export class ComercioCorresponsal extends Vendedor {
+  constructor(public provinciasDondeTrabaja: Provincia[]) {
+    super();
+  }
+
+  puedeTrabajarEn(ciudad: Ciudad): boolean {
+    return any((p) => p == ciudad.provincia, this.provinciasDondeTrabaja);
+  }
+
+  esInfuyente(): boolean {
+    return false;
   }
 }
 
