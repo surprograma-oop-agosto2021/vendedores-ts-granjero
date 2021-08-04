6d7953ad6342806516ffd8661e29b3d2f8f2a356
juanmiguel <juanmiguel.lombardisuzzi@gmail.comm>
2021-08-03 22:02:43 -0300

    pretirizado

--- src/vendedores.test.ts
+++ src/vendedores.test.ts
@@ -1,65 +1,75 @@
-import { Ciudad, Provincia, VendedorFijo, Viajante, ComercioCorresponsal } from "./vendedores";
+import {
+  Ciudad,
+  Provincia,
+  VendedorFijo,
+  Viajante,
+  ComercioCorresponsal,
+} from "./vendedores";
 
 describe("Vendedores", () => {
-	//Provincias
-	const buenosAires = new Provincia(8);
-	const tucuman = new Provincia(2);
-	const salta = new Provincia(3);
-	const cordoba = new Provincia(5);
-	//Ciudades
-	const sierra = new Ciudad(buenosAires);
-	const suipacha = new Ciudad(buenosAires);
-	const laPlata = new Ciudad(buenosAires);
-	const tafiDelValle = new Ciudad(tucuman);
-	const concepcion = new Ciudad(tucuman);
-	const yerbaBuena = new Ciudad(tucuman);
-	const cafayate = new Ciudad(salta);
-	const laMerced = new Ciudad(salta);
-	const tartagal = new Ciudad(salta);
+  //Provincias
+  const buenosAires = new Provincia(8);
+  const tucuman = new Provincia(2);
+  const salta = new Provincia(3);
+  const cordoba = new Provincia(5);
+  //Ciudades
+  const sierra = new Ciudad(buenosAires);
+  const suipacha = new Ciudad(buenosAires);
+  const laPlata = new Ciudad(buenosAires);
+  const tafiDelValle = new Ciudad(tucuman);
+  const concepcion = new Ciudad(tucuman);
+  const yerbaBuena = new Ciudad(tucuman);
+  const cafayate = new Ciudad(salta);
+  const laMerced = new Ciudad(salta);
+  const tartagal = new Ciudad(salta);
 
-	describe("1 - puede trabajar", () => {
-		describe("vendedor fijo", () => {
-			const vendedorFijo = new VendedorFijo(sierra);
-			
-			it("en la ciudad donde vive", () => {
-				expect(vendedorFijo.puedeTrabajarEn(sierra)).toBeTruthy();
-			});
-			
-			it("en otra ciudad", () => {
-				expect(vendedorFijo.puedeTrabajarEn(tafiDelValle)).toBeFalsy();
-			});
-			
-			it("No es influyente porque no puede serlo por definición", () => {
-				expect(vendedorFijo.vendedorInfuyente()).toBeFalsy();
-			})
-		});
-		
-		describe("viajante", () => {
-			const viajante = new Viajante([tucuman, salta, cordoba]);
-			
-			it("una ciudad que queda en una provincia donde trabaja", () => {
-				expect(viajante.puedeTrabajarEn(tafiDelValle)).toBeTruthy();
-			});
-			
-			it("una ciudad que queda en una provincia donde no trabaja", () => {
-				expect(viajante.puedeTrabajarEn(sierra)).toBeFalsy();
-			});
+  describe("1 - puede trabajar", () => {
+    describe("vendedor fijo", () => {
+      const vendedorFijo = new VendedorFijo(sierra);
 
-			it("es influyente", () => {
-				expect(viajante.vendedorInfuyente()).toBeTruthy();
-			})
-		});
+      it("en la ciudad donde vive", () => {
+        expect(vendedorFijo.puedeTrabajarEn(sierra)).toBeTruthy();
+      });
 
-		describe("comercio corresponsal", () => {
-			const corresponsal = new ComercioCorresponsal([suipacha, cafayate]);
-			
-			it("una ciudad que queda en una provincia donde trabaja", () => {
-				expect(corresponsal.puedeTrabajarEn(suipacha)).toBeTruthy();
-			});
-			
-			it("una ciudad que queda en una provincia donde no trabaja", () => {
-				expect(corresponsal.puedeTrabajarEn(sierra)).toBeFalsy();
-			});
-		});
-	});
+      it("en otra ciudad", () => {
+        expect(vendedorFijo.puedeTrabajarEn(tafiDelValle)).toBeFalsy();
+      });
+
+      it("No es influyente porque no puede serlo por definición", () => {
+        expect(vendedorFijo.vendedorInfuyente()).toBeFalsy();
+      });
+    });
+
+    describe("viajante", () => {
+      const viajante = new Viajante([tucuman, salta, cordoba]);
+
+      it("una ciudad que queda en una provincia donde trabaja", () => {
+        expect(viajante.puedeTrabajarEn(tafiDelValle)).toBeTruthy();
+      });
+
+      it("una ciudad que queda en una provincia donde no trabaja", () => {
+        expect(viajante.puedeTrabajarEn(sierra)).toBeFalsy();
+      });
+
+      it("es influyente", () => {
+        expect(viajante.vendedorInfuyente()).toBeTruthy();
+      });
+    });
+
+    describe("comercio corresponsal", () => {
+      const corresponsal = new ComercioCorresponsal([suipacha, cafayate]);
+
+      it("una ciudad que queda en una provincia donde trabaja", () => {
+        expect(corresponsal.puedeTrabajarEn(suipacha)).toBeTruthy();
+      });
+
+      it("una ciudad que queda en una provincia donde no trabaja", () => {
+        expect(corresponsal.puedeTrabajarEn(sierra)).toBeFalsy();
+      });
+
+      it("es influyente", () => {
+        expect(corresponsal.vendedorInfuyente()).toBeFalsy();
+      });
+    });
+  });
 });
--- src/vendedores.ts
+++ src/vendedores.ts
@@ -2,65 +2,72 @@ import { any, sum } from "ramda";
 
 // Clase Vendedor
 abstract class Vendedor {
-	abstract puedeTrabajarEn(ciudad: Ciudad): boolean;
-	abstract vendedorInfuyente(): boolean; // jm
+  abstract puedeTrabajarEn(ciudad: Ciudad): boolean;
+  abstract vendedorInfuyente(): boolean; // jm
 }
 
-// Clase VendedorFijo: se sabe en qué ciudad vive. 
+// Clase VendedorFijo: se sabe en qué ciudad vive.
 // No puede ser VendedorInfluyente
 export class VendedorFijo extends Vendedor {
-	constructor(public ciudadOrigen: Ciudad) {
-		super();
-	}
+  constructor(public ciudadOrigen: Ciudad) {
+    super();
+  }
 
-	//Metodo puedeTrabajarEn: devuelve V o F
-	puedeTrabajarEn(ciudad: Ciudad): boolean {
-		return ciudad == this.ciudadOrigen; // this.ciudadOrigen viene del constructor
-	}
+  //Metodo puedeTrabajarEn: devuelve V o F
+  puedeTrabajarEn(ciudad: Ciudad): boolean {
+    return ciudad == this.ciudadOrigen; // this.ciudadOrigen viene del constructor
+  }
 
-	vendedorInfuyente(): boolean {
-		return false;
-	}
+  vendedorInfuyente(): boolean {
+    return false;
+  }
 }
 
 // Clase Viajante: cada viajante está habilitado para trabajar en algunas provincias, se sabe cuáles son.
 export class Viajante extends Vendedor {
-	constructor(public provinciasDondeTrabaja: Provincia[]) {
-		super();
-	}
-// any es una funcion de ramda
-	puedeTrabajarEn(ciudad: Ciudad): boolean {
-		return any((p) => p == ciudad.provincia, this.provinciasDondeTrabaja);  
-	}
+  constructor(public provinciasDondeTrabaja: Provincia[]) {
+    super();
+  }
+  // any es una funcion de ramda
+  puedeTrabajarEn(ciudad: Ciudad): boolean {
+    return any((p) => p == ciudad.provincia, this.provinciasDondeTrabaja);
+  }
 
-	// true si la población sumanda de las provincias donde está habilitado >= 10 millones.
-	vendedorInfuyente(): boolean {
-		let poblacionSumada = 0;
-		this.provinciasDondeTrabaja.forEach(provincia => poblacionSumada += provincia.poblacion);
-		
-		return poblacionSumada >= 10 ? true : false;
-	}
+  // true si la población sumanda de las provincias donde está habilitado >= 10 millones.
+  vendedorInfuyente(): boolean {
+    let poblacionSumada = 0;
+    this.provinciasDondeTrabaja.forEach(
+      (provincia) => (poblacionSumada += provincia.poblacion)
+    );
+
+    return poblacionSumada >= 10 ? true : false;
+  }
 }
 
-// Clase ComercioCorresponsal: 
+// Clase ComercioCorresponsal:
 export class ComercioCorresponsal extends Vendedor {
-	constructor(public tieneSucursalEn: Ciudad[]) {
-		super();
-	}
+  constructor(public tieneSucursalEn: Ciudad[]) {
+    super();
+  }
 
-	puedeTrabajarEn(ciudad: Ciudad): boolean {
-		return any((p) => p == ciudad, this.tieneSucursalEn);
-	}
+  puedeTrabajarEn(ciudad: Ciudad): boolean {
+    return any((p) => p == ciudad, this.tieneSucursalEn);
+  }
 
-	vendedorInfuyente(): boolean {
-		return false;
-	}
+  vendedorInfuyente(): boolean {
+    const sucursales = this.tieneSucursalEn;
+
+    if (sucursales.length >= 5) {
+      return true;
+    }
+    return false;
+  }
 }
 
 export class Provincia {
-	constructor(public poblacion: number) {}
+  constructor(public poblacion: number) {}
 }
 
 export class Ciudad {
-	constructor(public provincia: Provincia) {}
+  constructor(public provincia: Provincia) {}
 }
